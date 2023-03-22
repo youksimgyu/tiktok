76fe618b9ce35aff07652bd851d29770465fb9cd
youksimgyu <dbrtlarb@gmail.com>
2023-03-22 19:58:13 +0900

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'constants/sizes.dart';
+import 'features/authentication/sign_up_screen.dart';
+
 void main() {
   runApp(const TikTokApp());
 }
@@ -12,9 +15,21 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
-      home: Container(),
+      home: const SingUpScreen(),
     );
   }
 }
