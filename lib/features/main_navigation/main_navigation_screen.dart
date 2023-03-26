df15c6c22bdce1a22fc446d05d4bd5b4e554bc10
youksimgyu <dbrtlarb@gmail.com>
2023-03-26 20:21:31 +0900

    메인바텀네비게이션바 커스텀 디자인

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok/features/main_navigation/widgets/post_video_button.dart';
+
+import '../../constants/gaps.dart';
+import '../../constants/sizes.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({Key? key}) : super(key: key);
@@ -13,19 +18,37 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   final screens = [
     const Center(
-      child: Text('Home'),
+      child: Text(
+        'Home',
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text('Search'),
+      child: Text(
+        'Discover',
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
+    ),
+    Container(),
+    const Center(
+      child: Text(
+        'Inbox',
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text('Search'),
-    ),
-    const Center(
-      child: Text('Search'),
-    ),
-    const Center(
-      child: Text('Search'),
+      child: Text(
+        'Profile',
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
   ];
 
@@ -35,28 +58,90 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     });
   }
 
+  void _onPostVideoButton() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Record video',
+            ),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: const [
-          NavigationDestination(
-              icon: FaIcon(
-                FontAwesomeIcons.house,
-                color: Colors.black38,
-              ),
-              label: 'Home'),
-          NavigationDestination(
-              icon: FaIcon(
-                FontAwesomeIcons.magnifyingGlass,
-                color: Colors.black38,
-              ),
-              label: 'Search'),
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: screens.elementAt(_selectedIndex),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: screens.elementAt(_selectedIndex),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: screens.elementAt(_selectedIndex),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: screens.elementAt(_selectedIndex),
+          ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(
+            Sizes.size12,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: 'Home',
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: 'Discover',
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onTap(1),
+              ),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButton,
+                child: const PostVideoButton(),
+              ),
+              Gaps.h24,
+              NavTab(
+                text: 'Inbox',
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: 'Profile',
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final IconData selectedIcon;
+  final Function onTap;
+
+  const NavTab({
+    Key? key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.selectedIcon,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 100),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  isSelected ? selectedIcon : icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
