ce8584a980dcb3cbccaaf6a17f27bf6a35f8d47d
youksimgyu <dbrtlarb@gmail.com>
2023-03-26 20:21:56 +0900

    동영상 찍는 버튼 연결

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          right: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xff61D4F0),
+              borderRadius: BorderRadius.circular(Sizes.size8),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 20,
+          child: Container(
+            height: 30,
+            width: 25,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(Sizes.size8),
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          height: 30,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(Sizes.size6),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: 18,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
