285836dfd6a8e673b00fe97103cd3b2e7bebad62
youksimgyu <dbrtlarb@gmail.com>
2023-06-14 19:30:08 +0900

    const 추가

---
+++ lib/constants/breakpoints.dart
@@ -0,0 +1,7 @@
+class Breakpoints {
+  static const double sm = 640;
+  static const double md = 768;
+  static const double lg = 1024;
+  static const double xl = 1280;
+  static const double xxl = 1536;
+}
