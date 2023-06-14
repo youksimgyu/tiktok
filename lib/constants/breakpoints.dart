db4b428fdbbcf12c5dfee028ef97b94f96ef2101
youksimgyu <dbrtlarb@gmail.com>
2023-06-14 23:23:21 +0900

    수정

--- lib/constants/breakpoints.dart
+++ lib/constants/breakpoints.dart
@@ -1,7 +1,7 @@
 class Breakpoints {
-  static const double sm = 640;
-  static const double md = 768;
-  static const double lg = 1024;
-  static const double xl = 1280;
-  static const double xxl = 1536;
+  static const double sm = 640.0;
+  static const double md = 768.0;
+  static const double lg = 1024.0;
+  static const double xl = 1280.0;
+  static const double xxl = 1536.0;
 }
